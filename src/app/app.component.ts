8bd3b89b26d2f1e51bb25f6858f00acd4a2872c2
Felix <josefeliciano.huamanvalqui@nttdata.com>
2021-10-14 23:08:28 -0500

    Adding auth lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shell';
+  isAuthenticated = false;
+
+  constructor(
+    private oidcSecurityService: OidcSecurityService
+  ) {
+  }
+  ngOnInit(): void {
+    this.oidcSecurityService.checkAuth().subscribe(({ isAuthenticated, userData, accessToken }) => {
+      //
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
+
+  onLogout(event: any): void {
+    event.preventDefault();
+    this.oidcSecurityService.logoff();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
+import { AuthConfigModule } from './auth/auth-config.module';
+import { JwtInterceptor } from './auth/jwt.interceptor';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthConfigModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
