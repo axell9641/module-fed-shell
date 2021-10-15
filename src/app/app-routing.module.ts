decc622b4f2fb845d749db3b0ce13c3004a54790
Felix <josefeliciano.huamanvalqui@nttdata.com>
2021-10-14 23:52:13 -0500

    Configure webpack III

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
+import { loadRemoteModule } from '@angular-architects/module-federation-runtime';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutoLoginPartialRoutesGuard } from 'angular-auth-oidc-client';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+
+  // Your route here:
+
+  {
+    path: 'identity',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteName: 'mfeIdentity',
+        exposedModule: './Module',
+      }).then((m) => m.IdentityModule),
+      canLoad: [AutoLoginPartialRoutesGuard],
+  },
+
+  // {
+  //   path: '**',
+  //   component: NotFoundComponent,
+  // },
+
+  // DO NOT insert routes after this one.
+  // { path:'**', ...} needs to be the LAST one.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { APP_ROUTES } from './app.routes';
 import { AuthConfigModule } from './auth/auth-config.module';
 import { JwtInterceptor } from './auth/jwt.interceptor';
 import { HomeComponent } from './home/home.component';
@@ -17,9 +16,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AuthConfigModule,
-    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
